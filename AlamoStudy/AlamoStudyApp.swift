b77cc38abe8927fd1c75d0db3c3b7e85ba05d334
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-05-09 17:03:52 +0900

    first commit

---
+++ AlamoStudy/AlamoStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlamoStudyApp.swift
+//  AlamoStudy
+//
+//  Created by Hyeonsoo Kim on 2022/05/09.
+//
+
+import SwiftUI
+
+@main
+struct AlamoStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
